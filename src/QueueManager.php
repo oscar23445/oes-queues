0ce32b1822a0657397a72fa9b7ebf5dff4b5185a
oscarscudder <oscarcpr@gmail.com>
2024-09-11 20:34:57 -0400

    Initial commit

---
+++ src/QueueManager.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oes\Queues;
+
+class QueueManager {
+    public function dispatch($job) {
+        // Dispatch logic here
+    }
+}
---
+++ tests/QueueTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+ * QueueTest.php
+ * Created by oscarscudder
+ * Project Name currnetVersion
+ *
+ * Initial version by oscarscudder
+ * Initial version on 2024-09-11 7:42 PM
+ * Decsription:
+ */
